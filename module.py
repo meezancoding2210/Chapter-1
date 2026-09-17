ad482bace660f8e6836543005979eeebc03cdcb9
Meezan Ansari <meezanansari2210@gmail.com>
2026-09-17 16:49:19 +0530

    Pip,modules and comments

---
+++ module.py
@@ -0,0 +1,9 @@
+import pyjokes
+print("Here's a random joke for you:")
+joke = pyjokes.get_joke()
+# This prinnts a random joke from the pyjokes library.
+print(joke)
+""" multi-line comment
+so thankss 
+that was my program
+"""
---
+++ pip.py
@@ -0,0 +1,2 @@
+# pips are used to install and manage software packages written in Python. 
+# eg - write "pip install pyjokes" to install the pyjokes library in terminal.
